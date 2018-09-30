32940f669565102d4b7ccc255291ed7dd20a30b3
Nash Taylor <ntaylorwss@outlook.com>
2018-09-30 00:36:15 -0700

    Move public utils to top-level 'helpers' module to make it clear they're functions to assist with custom transformations

--- megatron/__init__.py
+++ megatron/__init__.py
@@ -1,6 +1,6 @@
 from . import layers
 from . import adapters
-from . import utils
 from . import visuals
 from .pipeline import Pipeline, load_pipeline
 from .nodes import InputNode, TransformationNode, wrappers
+from .utils import helpers
