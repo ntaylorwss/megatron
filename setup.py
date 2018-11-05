64a0328f4b7682172130d8ed89917658e11176d0
Nash Taylor <ntaylorwss@outlook.com>
2018-11-05 01:58:37 -0800

    version 0.4.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ Megatron is distributed under the MIT license.
 '''
 
 setup(name='Megatron',
-      version='0.3.6',
+      version='0.4.0',
       description='A computation graph library for feature engineering with Numpy data',
       long_description=long_description,
       author='Nash Taylor',
