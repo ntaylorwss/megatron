dfea20f7c2f0a6483ae7fb26d26bb984a0cc00b0
Nash Taylor <ntaylorwss@outlook.com>
2018-11-05 10:24:23 -0800

    Move metrics module to layers

--- megatron/layers/__init__.py
+++ megatron/layers/__init__.py
@@ -1,11 +1,13 @@
 from .core import Input, StatefulLayer, Lambda
 from . import image
+from . import metrics
 from . import missing
 from . import numeric
 from . import shaping
 from . import text
 from . import wrappers
 from .image import *
+from .metrics import *
 from .missing import *
 from .numeric import *
 from .shaping import *
